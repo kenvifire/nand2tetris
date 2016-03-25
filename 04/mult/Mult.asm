8b109af7572390331b8f48c2eff8cfd227fdc016
Zhu Weijie <kenvi.zhu@dianping.com>
2016-03-25 11:17:29 +0800

    almost mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// compute R2=R0+R1
+    @i
+    M=1
+    @R2
+    M=0
+(LOOP)
+	@i
+	D=M
+	@R0
+	A=M
+	D=D-A
+	@END
+	D;JGT
+	@R1
+	D=M
+	@R2
+	M=D+M
+	@i
+	M=M+1
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
+
+
