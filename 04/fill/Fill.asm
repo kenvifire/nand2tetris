43e2bdfa9d92daf7dc0e69bf1cbf6af236573d91
Zhu Weijie <kenvi.zhu@dianping.com>
2016-03-26 19:59:24 +0800

    finish fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -7,5 +7,41 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
-
 // Put your code here.
+(START)
+	@color
+	M=0
+	@24576
+	D=M
+	@INIT
+	D;JEQ
+	@color
+	M=-1
+(INIT)
+	@count
+	M=0
+	@SCREEN
+	D=A
+	@address
+	M=D
+(DRAW)
+	@count
+	M=M+1
+	D=M
+	@8192
+	D=D-A
+	@START
+	D;JEQ
+	@color
+	D=M
+	@address
+	A=M
+	M=D
+	@address
+	M=M+1
+	@DRAW
+	0;JMP
+	
+
+
+
